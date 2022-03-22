6b7e23c16f99fcffadc3b9e924c6bfeb45747f68
sauron65 <jdaily0004@mymail.lausd.net>
2022-03-22 11:58:24 -0700

    change texture generation

--- rr.js
+++ rr.js
@@ -9,9 +9,10 @@ dv.setUint16(0, width);
 dv.setUint16(2, height);
 
 for (let i = 0; i < 256 * 4; i += 4) {
-  dv.setUint8(4 + i, 128);
-  dv.setUint8(5 + i, 128);
-  dv.setUint8(6 + i, 128);
+  const v = (Math.random() * 108) + 20;
+  dv.setUint8(4 + i, v);
+  dv.setUint8(5 + i, ((i - (Math.random() * 256 * 4)) < 3) && (Math.random() > 40) ? ((v + 40 > 255) ? 255 : v + 40) : v);
+  dv.setUint8(6 + i, v);
   dv.setUint8(7 + i, 255);
 }
 await fs.writeFile('./x.boi', dv);
